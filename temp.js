c5fcf0a481a1399ab3e8cefedb022dde537e759b
dhrrruvin <dhruvin.kanani.53@google.com>
2022-01-04 12:22:43 +0530

    base

---
+++ temp.js
@@ -0,0 +1,37 @@
+
+function insertRecord() {
+    
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '#Mysqlaccount1',
+        database: 'rto1'
+    });
+
+    var name = document.getElementById('name');
+    var address = document.getElementById('address');
+
+    console.log(name + ' ' + address);
+
+    if(name.length != 0 && address.length != 0) {
+
+        connection.connect(function (err) {
+            if(err) throw err;
+            console.log('Connected!');
+
+            var sql = "INSERT INTO customers values(" +"\""+ name  +"\"" + ","+ "\"" + address +"\"" + ");";
+
+            connection.query(sql, function(err, result) {
+                if(err) throw err;
+                console.log('1 row inserted!');
+            });
+        
+        });
+
+    }
+
+}
+
+const submitBtn = document.getElementById('submit');
+
+submitBtn.addEventListener("click", insertRecord);
