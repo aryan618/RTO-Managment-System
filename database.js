d4080b7ac112bef7193b6c28ba52eecebd8f11cf
Keshav Raj <www.keshav.mars@gmail.com>
2022-01-17 10:55:56 +0530

    dotenv support added

--- database.js
+++ database.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv').config();
 
 module.exports = mysql.createConnection({
-    host: 'localhost',
-    user: 'kevin',
-    password: 'keshav929',
-    database: 'rto1'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
